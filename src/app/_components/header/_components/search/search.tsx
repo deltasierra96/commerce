b9f024cb70510c814ee28e2e724c9f33e2573fe6
deltasierra96 <dylanstephens96@outlook.com>
2024-09-09 22:55:46 +0100

    search functionality

--- src/app/_components/header/_components/search/search.tsx
+++ src/app/_components/header/_components/search/search.tsx
@@ -2,7 +2,8 @@
 import { Container } from '@/components/ui/container';
 import { Drawer } from '@/components/ui/drawer';
 import { SearchInput } from '@/components/ui/search-input';
-import React from 'react';
+import Form from 'next/form';
+import { useSearchParams } from 'next/navigation';
 
 type SearchProps = {
   isOpen: boolean;
@@ -10,18 +11,23 @@ type SearchProps = {
 };
 
 export const Search = ({ isOpen, setOpen }: SearchProps) => {
+  const searchParams = useSearchParams();
   return (
     <Drawer position={'top'} size={'lg'} onOpenChange={setOpen} isOpen={isOpen}>
       <Drawer.Content>
         <Container>
-          <div className='py-2'>
-            <SearchInput
-              autoFocus
-              autoComplete='off'
-              hideLabel
-              name='search'
-              placeholder='What are you looking for today?'
-            />
+          <div className="py-2">
+            <Form action="/search" className="w-max-[550px] relative w-full lg:w-80 xl:w-full">
+              <SearchInput
+                type="text"
+                name="q"
+                defaultValue={searchParams?.get('q') || ''}
+                autoFocus
+                autoComplete="off"
+                hideLabel
+                placeholder="What are you looking for today?"
+              />
+            </Form>
           </div>
         </Container>
       </Drawer.Content>
