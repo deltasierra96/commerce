980d421a20637ec44b696775db1011cc89539df2
deltasierra96 <dylanstephens96@outlook.com>
2024-10-13 21:58:41 +0100

    .

--- src/app/_components/header/_components/mobile-header/_components/navigation/_components/navigation-menu.tsx
+++ src/app/_components/header/_components/mobile-header/_components/navigation/_components/navigation-menu.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { ButtonLink } from '@/components/ui/button';
 import { Menu } from '@/shopify/types';
+import { Link } from 'react-aria-components';
 
 type NavigationMenuProps = {
   menu: Menu;
@@ -10,9 +11,18 @@ export const NavigationMenu = ({ menu, ...props }: NavigationMenuProps) => {
   return (
     <div className="space-y-4 p-6">
       {menu.items?.map((menuItem) => (
-        <ButtonLink key={menuItem.title} block href={menuItem.url}>
-          {menuItem.title}
-        </ButtonLink>
+        <div key={menuItem.id}>
+          <ButtonLink block href={menuItem.url}>
+            {menuItem.title}
+          </ButtonLink>
+          {menuItem.items?.map((menuItemSub) => {
+            return (
+              <Link className={'flex'} key={menuItemSub.id} href={menuItemSub.url}>
+                {menuItemSub.title}
+              </Link>
+            );
+          })}
+        </div>
       ))}
     </div>
   );
