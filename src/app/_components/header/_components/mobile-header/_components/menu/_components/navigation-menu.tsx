538ca7b1141c70d4bf4f4e713c685f5fbf99d4d2
deltasierra96 <dylanstephens96@outlook.com>
2024-10-15 22:35:10 +0100

    .

--- src/app/_components/header/_components/mobile-header/_components/menu/_components/navigation-menu.tsx
+++ src/app/_components/header/_components/mobile-header/_components/menu/_components/navigation-menu.tsx
@@ -1,44 +1,234 @@
 'use client';
+import { Icon } from '@/components/ui/icon';
 import { Menu } from '@/shopify/types';
-import { Link } from 'react-aria-components';
+import { AnimatePresence, Variants, motion } from 'framer-motion';
+import { useState } from 'react';
+
+export const variants: Variants = {
+  'in-view': { x: '0px', opacity: 1, transition: { type: 'tween', ease: 'easeOut' } },
+  'out-of-view': (index: number) => ({
+    x: index > 0 ? '250px' : '-250px',
+    opacity: index > 0 ? 1 : 0,
+    transition: { type: 'tween', ease: 'easeOut' }
+  })
+};
 
 type NavigationMenuProps = {
   menu: Menu;
 };
 
-export const NavigationMenu = ({ menu, ...props }: NavigationMenuProps) => {
-  return (
-    <div className="divide-y divide-neutral-100">
-      {menu.items?.map((menuItem) => (
-        <div className="space-y-4 p-4" key={menuItem.id}>
-          <Link className={'flex underline'} href={menuItem.url}>
-            {menuItem.title}
-          </Link>
-          {menuItem.items?.map((menuItemSub) => {
-            return (
-              <div className="bg-red-50 pl-5">
-                <Link className={'flex py-2'} key={menuItemSub.id} href={menuItemSub.url}>
-                  {menuItemSub.title}
-                </Link>
+const navigationData = [
+  {
+    id: Math.random(),
+    label: 'Link 1 (level 1)',
+    links: [
+      {
+        id: Math.random(),
+        label: 'Link 1 (level 2)',
+        links: [
+          {
+            id: Math.random(),
+            label: 'Link 1 (level 3)'
+          },
+          {
+            id: Math.random(),
+            label: 'Link 2 (level 3)'
+          },
+          {
+            id: Math.random(),
+            label: 'Link 3 (level 3)'
+          }
+        ]
+      },
+      {
+        id: Math.random(),
+        label: 'Link 2 (level 2)'
+      },
+      {
+        id: Math.random(),
+        label: 'Link 3 (level 2)'
+      }
+    ]
+  },
+  {
+    id: Math.random(),
+    label: 'Link 2 (level 1)',
+    links: [
+      {
+        id: Math.random(),
+        label: 'Link 1 (level 2)',
+        links: [
+          {
+            id: Math.random(),
+            label: 'Link 1 (level 3)'
+          },
+          {
+            id: Math.random(),
+            label: 'Link 2 (level 3)'
+          },
+          {
+            id: Math.random(),
+            label: 'Link 3 (level 3)'
+          }
+        ]
+      },
+      {
+        id: Math.random(),
+        label: 'Link 2 (level 2)'
+      },
+      {
+        id: Math.random(),
+        label: 'Link 3 (level 2)'
+      }
+    ]
+  },
+  {
+    id: Math.random(),
+    label: 'Link 3 (level 1)',
+    links: [
+      {
+        id: Math.random(),
+        label: 'Link 1 (level 2)',
+        links: [
+          {
+            id: Math.random(),
+            label: 'Link 1 (level 3)'
+          },
+          {
+            id: Math.random(),
+            label: 'Link 2 (level 3)'
+          },
+          {
+            id: Math.random(),
+            label: 'Link 3 (level 3)'
+          }
+        ]
+      },
+      {
+        id: Math.random(),
+        label: 'Link 2 (level 2)'
+      },
+      {
+        id: Math.random(),
+        label: 'Link 3 (level 2)'
+      }
+    ]
+  }
+];
 
-                <div className="bg-green-50 pl-10">
-                  {menuItemSub.items?.map((menuItemSubSub) => {
-                    return (
-                      <Link
-                        className={'flex py-2'}
-                        key={menuItemSubSub.id}
-                        href={menuItemSubSub.url}
-                      >
-                        {menuItemSubSub.title}
-                      </Link>
-                    );
-                  })}
-                </div>
-              </div>
+export const NavigationMenu = ({ menu, ...props }: NavigationMenuProps) => {
+  const [selectedItems, setSelectedItems] = useState<any[]>([]);
+  console.log('selectedItems', selectedItems);
+
+  const goToNextLevel = (item: any) => {
+    if (!item.links) {
+      return;
+    }
+    setSelectedItems([...selectedItems, item.id]);
+  };
+
+  const goBack = () => {
+    const selectedItemsClone = [...selectedItems];
+    selectedItemsClone.pop();
+    setSelectedItems([...selectedItemsClone]);
+  };
+
+  const getNavItems = (selectedItems: string[]) => {
+    let result: any[] = [];
+    let links: any[] = [...navigationData];
+    console.log('links working', links);
+    let itr = 0;
+
+    if (selectedItems.length === 0) {
+      return navigationData;
+    }
+
+    // We will run the loop until we reach the correct level
+    while (itr < selectedItems.length) {
+      let selectedLink: any;
+
+      // Finding the selected item for this level
+      for (let i = 0; i < links.length; i++) {
+        console.log('merp working');
+        if (links[i].id === selectedItems[itr]) {
+          selectedLink = links[i];
+
+          // We keep a track of the next level links
+          if (selectedLink.links) {
+            result = [...selectedLink.links];
+          }
+          break;
+        }
+      }
+      links = [...result];
+      itr++;
+    }
+    return result;
+  };
+
+  return (
+    <div className="relative flex flex-col">
+      <nav className="relative mt-24">
+        <motion.ul
+          variants={variants}
+          initial="in-view"
+          animate={selectedItems.length > 0 ? 'out-of-view' : 'in-view'}
+          custom={selectedItems.length > 0 ? -1 : 0}
+          className="top-0 w-full duration-200"
+        >
+          {/* First level items */}
+          {navigationData?.map((item: any) => {
+            return (
+              <li key={item.id} className="px-4 py-2">
+                <button
+                  onClick={() => goToNextLevel(item)}
+                  className="flex w-full flex-row items-center"
+                >
+                  <span className="pr-2">{item.label}</span>
+                  {item.links && <Icon icon="chevron-right" />}
+                </button>
+              </li>
             );
           })}
-        </div>
-      ))}
+        </motion.ul>
+
+        {/* Subsequent levels */}
+        <AnimatePresence>
+          {selectedItems.length > 0 &&
+            selectedItems.map((id, index) => {
+              return (
+                <motion.ul
+                  key={id}
+                  variants={variants}
+                  initial="out-of-view"
+                  animate={index + 1 === selectedItems.length ? 'in-view' : 'out-of-view'}
+                  exit="out-of-view"
+                  custom={index + 1 === selectedItems.length ? 1 : -1}
+                  className="top-0 w-full duration-200"
+                >
+                  <li className="pb-4">
+                    <button className="flex items-center" onClick={goBack}>
+                      <Icon icon="chevron-left" /> <span className="pl-2">Back</span>
+                    </button>
+                  </li>
+                  {getNavItems(selectedItems.slice(0, index + 1))?.map((item: any) => {
+                    return (
+                      <li key={item.id} className="px-4 py-2">
+                        <button
+                          onClick={() => goToNextLevel(item)}
+                          className="flex w-full flex-row items-center"
+                        >
+                          <span className="pr-2">{item.label}</span>
+                          {item.links && <Icon icon="chevron-right" />}
+                        </button>
+                      </li>
+                    );
+                  })}
+                </motion.ul>
+              );
+            })}
+        </AnimatePresence>
+      </nav>
     </div>
   );
 };
--- src/app/_components/header/_components/mobile-header/_components/menu/menu.tsx
+++ src/app/_components/header/_components/mobile-header/_components/menu/menu.tsx
@@ -2,8 +2,10 @@
 import { ButtonIcon } from '@/components/ui/button-icon';
 import { DialogHeader } from '@/components/ui/dialog';
 import { Drawer } from '@/components/ui/drawer';
+import { Separator } from '@/components/ui/separator';
 import { Menu as ShopifyMenu } from '@/shopify/types';
 import { NavigationMenu } from './_components/navigation-menu';
+import { Derp } from './_components/navigation-menu-3';
 
 type MenuProps = {
   menu: ShopifyMenu;
@@ -14,10 +16,12 @@ export const Menu = ({ menu, ...props }: MenuProps) => {
     <Drawer position="left" size={'lg'} {...props}>
       <ButtonIcon aria-label="Open mobile navigation" icon="menu" variant="ghost" color="neutral" />
       <Drawer.Content>
-        <div className="flex flex-col h-full">
+        <div className="flex h-full flex-col">
           <DialogHeader>Menu</DialogHeader>
-          <div className="flex flex-col flex-1 min-h-0 overflow-y-scroll scrollbar-thin scrollbar-track-neutral-50 scrollbar-thumb-neutral-200">
+          <div className="scrollbar-thin scrollbar-track-neutral-50 scrollbar-thumb-neutral-200 flex min-h-0 flex-1 flex-col overflow-y-scroll">
             <NavigationMenu menu={menu} />
+            <Separator />
+            <Derp menu={menu} />
           </div>
         </div>
       </Drawer.Content>
