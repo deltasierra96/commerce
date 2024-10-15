99ff542b98d7a49b39237be7938c4488f7359e43
deltasierra96 <dylanstephens96@outlook.com>
2024-10-15 18:34:00 +0100

    .

--- src/app/_components/header/_components/desktop-header/_components/navigation/navigation.tsx
+++ src/app/_components/header/_components/desktop-header/_components/navigation/navigation.tsx
@@ -1,6 +1,6 @@
 import { MAIN_MENU_HANDLE } from '@/app/constants';
 import { getMenu } from '@/shopify/getMenu';
-import { NavigationMenu } from './_components/navigation-menu';
+import { NavigationMenu } from './_components/navigation-menu-hover';
 
 export const Navigation = async () => {
   const menu = await getMenu(MAIN_MENU_HANDLE);
